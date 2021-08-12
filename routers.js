584c892f8c241f78a749b63c58390facf50aa63a
Malluru Swathi <87529946+Swathi-25@users.noreply.github.com>
2021-08-12 15:37:06 +0530

    Update routers.js

--- routers.js
+++ routers.js
@@ -1,57 +1,66 @@
 let express = require('express');  
-let app=express();
 let router=express.Router();
 let bodyParser = require('body-parser');
 let fs=require('fs');
 let axios = require("axios");
 let path = require('path');
+const pro=require('./mockunit');
+
+function product(num,num2){
+ 
+    return num*num2;
+    
+}
+
+function nonRepeatingchar(str){
+  let result;
+  for (let i = 0; i < str.length; i++) {
+      if(str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
+         result= str.charAt(i);
+      }
+      
+  }
+  return result;
+}
+
+
+
 
 //to use route /file that outputs a file content from any local directory
 
-router.get('/', function(req, res){
-  let options = {
-      root: path.join(__dirname)
-  };
-    
-  let fileName = 'data.txt';
-  res.sendFile(fileName, options, function (err) {
-      if (err) {
-          next(err);
-      } else {
-          console.log('Sent:', fileName);
-      }
-  });
+router.get('/file', function(req, res){
+  fs.readFile('data.txt', function (err, data) {
+    if (err) return res.send(err);
+    res.send(data.toString());
+ });
 });
  
+
 
 //to use route /product for finding a product of given url parameters - num & num2
 router.get("/product", function (req, res) {
     let num = req.query.num;
     let num2= req.query.num2;
     if(num&&num2){
-    let result= (num*num2);
+    let result= product(num,num2);
+    res.json(
+      {"the product of two params ": result}
+      );
     }
-    else res.json(
+    else {
+       res.json(
       {"error":"provide two values"}
       );
-  res.json(
-    {"the product of two params ": result}
-    );
+    }
+    console.log(result);
+  
 })
+
 
 
 // to use route /nonrepeatingchars API for finding the first nonrepeating character in a String
 
-function nonRepeatingchar(str){
-    let result;
-    for (let i = 0; i < str.length; i++) {
-        if(str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
-           result= str.charAt(i);
-        }
-        
-    }
-    return result;
-}
+
 router.get("/nonrepeatingchars",function(req,res){
     let s=req.query.s;
      if(s){
@@ -68,11 +77,13 @@ router.get("/nonrepeatingchars",function(req,res){
 })
 
 
+
 // to use route /requestbody Api that will accept a body which need to be written in a file
 
 router.post("/requestbody", function (req, res) {
     let data=req.body;
-    fs.writeFile('data.txt',data.data,function(err){
+    if(req.body){
+    fs.writeFile('data.txt',data.name,function(err){
       if(err){
         res.json({
           "Error": "File cant be written"
@@ -87,21 +98,24 @@ router.post("/requestbody", function (req, res) {
       }
       
     })
+    }
 
   })
 
-  //to use route /json that call a URL and outputs the respose of api
+  
 
+  //to use route /json that call a URL and outputs the respose of api
   router.get('/json',function(req,res){
 
     axios.get("https://jsonplaceholder.typicode.com/todos").then(function(response) {
     
-          res.json(response.data)
-        }).catch(function(error) {
-          res.json("Error occured!")
+          res.json(response.data);
+        }).catch(function(err) {
+          res.json({"err":"Error occured!"})
         })
     })
 
 
-
-module.exports=router
+module.exports.router=router;
+module.exports.nonRepeatingchar=nonRepeatingchar;
+module.exports.product=product;
