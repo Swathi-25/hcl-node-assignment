96f81fb6fdf8762a926c6ef2315476b469290928
Malluru Swathi <87529946+Swathi-25@users.noreply.github.com>
2021-08-12 15:37:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.set("json spaces", 2)
 app.use(express.json());
 app.use('/approutes',route);
 
-let server = app.listen(3000, function () {  
-  let host = server.address().address;  
-  let port = server.address().port;  
-  console.log('app listening at http://%s:%s', host, port);  
-});  
+app.listen(3000); 
+ 
+  
